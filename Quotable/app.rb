516bb6ab00dbc7784c52f50406e61719f53cefb9
itggot-Joel-Hilmersson <joel.batsmanhilmersson@elev.ga.ntig.se>
2020-01-30 14:24:04 +0100

    Har lagt till starsida där man kan registrera och logga in

--- Quotable/app.rb
+++ Quotable/app.rb
@@ -3,4 +3,13 @@ require "sinatra"
 require "sqlite3"
 require "bcrypt"
 enable :sessions
+db = SQLite3::Database.new("db/todo_login.db")
+db.results_as_hash = true
 
+get("/") do 
+    slim(:index)
+end
+
+post("/register") do
+    slim(:"user/new")
+end
