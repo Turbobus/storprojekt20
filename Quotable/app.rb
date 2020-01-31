b4308fa2b42aeaf8452a24b9fe0ac69356c97eee
itggot-Joel-Hilmersson <joel.batsmanhilmersson@elev.ga.ntig.se>
2020-01-31 12:43:47 +0100

    Lagt till olika slim-filer

--- Quotable/app.rb
+++ Quotable/app.rb
@@ -21,3 +21,24 @@ end
 get("/quotes") do 
     slim(:"quotes/index")
 end
+
+post("/register") do 
+    username = params[:username]
+    password = params[:password]
+    password_verify = params[:password_verify]
+    starting_quota = 1
+
+    if password != password_verify || username == "" || password == ""
+        redirect("/password_do_not_match")
+    end
+
+    exist = db.execute("SELECT username FROM user WHERE username LIKE ?", username)
+    password_scramble = BCrypt::Password.create(password)
+
+    if exist.empty?
+        db.execute("INSERT INTO user(username, password, quota) VALUES(?, ?)", username, password_scramble, starting_quota)
+    else
+        redirect("/username_exist")
+    end
+    redirect("/quotes")
+end
