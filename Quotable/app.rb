1ec46a294d7b511376724cfadd7ed8c9cbf2ac9f
itggot-Joel-Hilmersson <joel.batsmanhilmersson@elev.ga.ntig.se>
2020-02-06 15:44:09 +0100

    Gjorde klart valideringen helt så att den fungerade för registreringen. Började på loggin verifieraren.

--- Quotable/app.rb
+++ Quotable/app.rb
@@ -10,31 +10,31 @@ get("/") do
     slim(:index)
 end
 
-get("/user/new") do
+get("/user/new/") do
     slim(:"user/new")
 end
 
-get("/user/index") do
-    slim(:"user/login")
+get("/user/") do
+    slim(:"user/index")
 end
 
-get("/quotes") do 
+get("/quotes/") do 
     slim(:"quotes/index")
 end
 
-post("/register") do 
+post("/user/new") do 
     username = params[:username]
     password = params[:password]
     password_verify = params[:password_verify]
     starting_quota = 1
 
-    if password != password_verify || username == "" || password == ""
+    if password != password_verify || username == "" || password == "" || username == ""
         redirect("/empty_or_do_not_match")                                      #Måste ändra   OBS!!!!!
     end
     
     only_integer = password.scan(/\D/).empty?
     only_letters = password.scan(/\d/).empty? 
-    if only_integer == true || only_letters == true || password.length > 3
+    if only_integer == true || only_letters == true || password.length < 4
         redirect("/only_integer_or_letter_under_lengt3")         #OBS ska ändras   Blir här när det bara är siffror eller bara bokstäver eller när det är under 4 tecken
     end
 
@@ -46,5 +46,26 @@ post("/register") do
     else
         redirect("/username_exist")                                            #Måste ändra   OBS!!!!!
     end
-    redirect("/")
+    redirect("/quotes/")
+end
+
+post("/user") do
+    username = params[:username]
+    password = params[:password]
+    session[:logged_in] = nil
+
+    #Kollar ifall Användaren finns
+    exist = db.execute("SELECT username FROM user WHERE username LIKE ?", username)
+    if exist.empty?
+        redirect("/Användare existerar inte")               #MÅSTE ändra OBS!!!!!!!!!!!!
+    end
+
+    #Hämtar användarens lödsenord för jämförelse
+    controll_password = db.execute("SELECT password FROM user WHERE username LIKE ?", username)[0]["password"]
+    if BCrypt::Password.new(controll_password) == password
+        session[:logged_in] = db.execute("SELECT user_id FROM user WHERE username LIKE ?", username)[0]["user_id"]
+        redirect("/quotes/")
+    else
+        redirect("/Password incorrect")                  #Måste ändra OBS!!!!!!!
+    end
 end
--- projektplan.md
+++ projektplan.md
@@ -1,12 +1,7 @@
 # Projektplan
 
 ## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra)
-En webbshop där användare ska kunna ha en egen kundvagn. Ifall man inte är inloggad kan man bara se på varorna och ifall man är Admin så kan man lägga till nya varor samt se de andra användarnas kundvagnar. Man ska kunna handla olika citat från olika människor. "Pengarna" eller Quota som det kommer heta tjänas genom att säga de olika quotsen du har köpt. Man börjar alltså med ett gratis qoute så att man kan säga det och får mer Quota (pengarna) så du kan köpa bättre qoutes som i sin tur ger dig yttligare mer pengar när du säger dem.
+En webbshop där användare ska kunna ha en egen kundvagn. Ifall man inte är inloggad kan man bara se på varorna och ifall man är Admin så kan man lägga till nya varor samt se de andra användarnas kundvagnar. Man ska kunna handla olika citat från olika människor. "Pengarna" eller Quota som det kommer heta tjänas genom att säga de olika quotsen du har köpt. Man börjar alltså med ett gratis qoute så att man kan säga det och får mer Quota (pengarna) så du kan köpa bättre qoutes som i sin tur ger dig yttligare mer pengar när du säger dem. Admin ska kunna lägga till eller ändra Quotes medan en vanlig användare kan bara göra det som jag beskrev innan. Ifall man inte är innloggad så ska man bara kunna se de olika quotsen som existerar.
 ## 2. Vyer (visa bildskisser på dina sidor)
 ## 3. Databas med ER-diagram (Bild)
 ## 4. Arkitektur (Beskriv filer och mappar - vad gör/inehåller de?)
-
-Hej detta är ett test okej
-
-Nu har vi lite fler saker här
-hmm varför fungerar det inte hahahah
