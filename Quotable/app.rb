30a1d8b602daaeeae9c3dc00190e4eda5e7eb9fa
itggot-Joel-Hilmersson <joel.batsmanhilmersson@elev.ga.ntig.se>
2020-02-06 14:29:32 +0100

    Har lagt till lite verifiering för lösenord

--- Quotable/app.rb
+++ Quotable/app.rb
@@ -3,7 +3,7 @@ require "sinatra"
 require "sqlite3"
 require "bcrypt"
 enable :sessions
-db = SQLite3::Database.new("db/todo_login.db")
+db = SQLite3::Database.new("db/quotables.db")
 db.results_as_hash = true
 
 get("/") do 
@@ -14,7 +14,7 @@ get("/user/new") do
     slim(:"user/new")
 end
 
-get("/user/login") do
+get("/user/index") do
     slim(:"user/login")
 end
 
@@ -29,7 +29,13 @@ post("/register") do
     starting_quota = 1
 
     if password != password_verify || username == "" || password == ""
-        redirect("/password_do_not_match")
+        redirect("/password_do_not_match")                                      #Måste ändra   OBS!!!!!
+    end
+    
+    only_integer = password.scan(/\D/).empty?
+    only_letters = password.scan(/\d/).empty? 
+    if only_integer == true || only_letters == true || password.length > 3
+        redirect("/password_do_not_match")         #OBS ska ändras   Blir här när det bara är siffror eller bara bokstäver eller när det är under 4 tecken
     end
 
     exist = db.execute("SELECT username FROM user WHERE username LIKE ?", username)
@@ -38,7 +44,7 @@ post("/register") do
     if exist.empty?
         db.execute("INSERT INTO user(username, password, quota) VALUES(?, ?)", username, password_scramble, starting_quota)
     else
-        redirect("/username_exist")
+        redirect("/username_exist")                                            #Måste ändra   OBS!!!!!
     end
-    redirect("/quotes")
+    redirect("/")
 end
