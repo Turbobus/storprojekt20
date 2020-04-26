97e4f19edae86e96eade5a58c8e56ac02181cdb0
itggot-Joel-Hilmersson <joel.batsmanhilmersson@elev.ga.ntig.se>
2020-04-26 23:16:38 +0200

    Gjort en massor med dokumentation enligt yardoc

--- Quotable/app.rb
+++ Quotable/app.rb
@@ -7,9 +7,10 @@ require_relative "./model.rb"
 require "sinatra/reloader"
 also_reload "./model.rb"
 enable :sessions
-include Model
+include Model #Makes this possible
 
-
+# Checks that the user is an admin before all the admin pages or otherwise redirects them
+#
 before /\/(quotes\/new\/|origin\/new\/|quotes\/edit\/|quotes\/edit\/update|quotes\/edit\/delete|origin\/edit\/|origin\/edit\/update)/ do
     admin = is_admin(session[:logged_in]) 
     if admin == false
@@ -17,24 +18,42 @@ before /\/(quotes\/new\/|origin\/new\/|quotes\/edit\/|quotes\/edit\/update|quote
     end
 end
 
+# Checks if user has logged in before the cart and library otherwise redirects them to the login page
+#
 before /\/(user\/show\/|cart\/)/ do 
     if session[:logged_in] == nil
         redirect("/user/")
     end
 end
 
+# Displays the landingpage
+#
 get("/") do 
     slim(:index)
 end
 
+# Displays the login page
+#
 get("/user/") do
     slim(:"user/index")
 end
 
+# Displays the register page
+#
 get("/user/new/") do
-    slim(:"user/new")
+    slim(:"user/new")                   #Kolla över verifieringen ifall man försöker uppdatera eller deleta quotes/origins så att man är admin
 end
 
+# Displays the users library
+#
+# @param [String] colums the column names delimited by (,space) 
+# @param [String] quotes the name of the first table
+# @param [String] library the name of the second table
+# @param [String] togheter what the two tables have togheter
+# @param [String] library.user_id a condition that looks at the user_id
+# @param [String] session[:logged_in] the user_id of the user
+#
+# @see Model#join_from_db
 get("/user/show/") do
     user_library = join_from_db("quotes.quote_id, quote, earnings", "quotes", "library", "library.quote_id = quotes.quote_id", "library.user_id", session[:logged_in])
     said_quote = session[:said_quote]
@@ -42,16 +61,38 @@ get("/user/show/") do
     slim(:"user/show", locals:{user_library: user_library, said_quote: said_quote})
 end
 
+# Displays the main quote store
+#
+# @param [String] columns the columns that is to be selected
+# @param [String] quotes the table that the information will come from
+#
+# @see Model#get_from_db
 get("/quotes/") do
     quotes = get_from_db("quote_id, quote, price", "quotes")
     slim(:"quotes/index", locals:{quotes: quotes})
 end
 
+# Displays the updating page for quotes
+#
+# @param [String] columns the columns that is to be selected
+# @param [String] quotes the table that the information will come from
+#
+# @see Model#get_from_db
 get("/quotes/edit/") do
     quotes = get_from_db("quote_id, quote, price, earnings, origin_id", "quotes")
     slim(:"quotes/edit", locals:{quotes: quotes})
 end
 
+# Displays a single quote
+#
+# @param [String] quote_id the column that is to be selected
+# @param [String] quotes the table that the information will come from
+# @param [String] condition a optional condition to be checked 
+# @param [Integer] :quote_id the displaying quotes id
+# @param [integer] session[:logged_in] the current users id
+#
+# @see Model#get_from_db
+# @see Model#owned_quote
 get("/quotes/:quote_id") do
     quote_id = params["quote_id"]
     found_quote = get_from_db("quote_id", "quotes", "quote_id", "#{quote_id}")
@@ -63,11 +104,27 @@ get("/quotes/:quote_id") do
     slim(:"quotes/show", locals:{quote_information: quote_information, quote_id: quote_id, owned: owned})
 end
 
+# Displays the page where an admin can add new quotes
+#
+# @param [String] columns the columns that is to be selected
+# @param [String] origin the table that the information will come from
+#
+# @see Model#get_from_db
 get("/quotes/new/") do
     origin = get_from_db("origin_id, person, backstory", "origin")
     slim(:"quotes/new", locals:{origin: origin})
 end
 
+# Displays the users shoppingcart
+#
+# @param [String] columns the column names delimited by (,space) 
+# @param [String] quotes the name of the first table
+# @param [String] cart the name of the second table
+# @param [String] togheter what they have togheter
+# @param [String] cart.user_id looks for the user_id
+# @param [String] session[:logged_in] the user_id from the user
+#
+# @see Model#join_from_db
 get("/cart/") do 
     user_cart = join_from_db("quotes.quote_id, quote, price, earnings", "quotes", "cart", "cart.quote_id = quotes.quote_id", "cart.user_id", session[:logged_in])
     
@@ -80,15 +137,38 @@ get("/cart/") do
     slim(:"cart/index", locals:{user_cart: user_cart, quote_ids: quote_ids, total_price: total_price})
 end
 
+# Displays the page where an admin can create new origins
+#
 get("/origin/new/") do 
     slim(:"origin/new") 
 end
 
+# Displays the page where an admin can edit existing origins
+#
+# @param [String] columns the columns that is to be selected
+# @param [String] origin the table that the information will come from
+#
+# @see Model#get_from_db
 get("/origin/edit/") do 
     origins = get_from_db("origin_id, person, backstory", "origin")
     slim(:"origin/edit", locals:{origins: origins})
 end
 
+# Tries to login the user
+#
+# @param [Array] session[:time_checker] Array containing the times when the user tries to login
+# 
+# @param [String] column the column/columns that is to be selected
+# @param [String] user the table that the information will come from
+# @param [String] condition_username a condition that looks for the username 
+# @param [String] username the inputed username
+#
+# @param [String] password the inputed password
+# @param [String] username the inputed username
+#
+# @see Model#time_checker
+# @see Model#get_from_db
+# @see Model#password_checker
 post("/user") do
     username = params[:username]
     password = params[:password]
@@ -124,6 +204,25 @@ post("/user") do
     end
 end
 
+# Regisers a new user
+#
+# @param [String] username the inputed username
+# @param [String] password the inputed password
+# @param [String] password_verify the verifier password
+#
+# @param [String] username the column that is to be selected
+# @param [String] user the table that the information will come from
+# @param [String] condition_username a condition that looks for the username 
+# @param [String] username the inputed username
+#
+# @param [String] user the table where the data should go
+# @param [String] columns which columns the data should go in
+# @param [String] "?,?,?" amount of columns in (?)
+# @param [Array] values containing the values to be put in the columns 
+#
+# @see Model#input_check
+# @see Model#get_from_db
+# @see Model#insert_into_db
 post("/user/new") do 
     username = params[:username]
     password = params[:password]
@@ -155,6 +254,20 @@ post("/user/new") do
     redirect("/quotes/")
 end
 
+# Adds the quota earned from saying the quote
+#
+# @param [String] earnings the column that is to be selected
+# @param [String] quotes the table that the information will come from
+# @param [String] condition_quote_id a condition that looks att quote_id 
+# @param [String] quote_id the value of the conition
+#
+# @param [String] user the table name
+# @param [String] colum the column name and what it should be updated to
+# @param [String] condition_user_id a condition that looks att the user_id
+# @param [String] session[:logged_in] the user_id of the user
+#
+# @see Model#get_from_db
+# @see Model#update_db
 post("/user/show") do 
     quote_id = params[:quote_id]
     earnings = get_from_db("earnings", "quotes", "quote_id", quote_id)[0]["earnings"]
@@ -165,6 +278,39 @@ post("/user/show") do
     redirect("/user/show/")
 end
 
+# Buy the users shoppingcart and adds them to the library
+#
+# @param [String] price the column name 
+# @param [String] quotes the name of the first table
+# @param [String] cart the name of the second table
+# @param [String] togheter what they have togheter
+# @param [String] cart.user_id a condition that looks for a specific user_id
+# @param [String] session[:logged_in] the specific user_id to be looked at
+#
+# @param [String] quota the column that is to be selected
+# @param [String] user the table that the information will come from
+# @param [String] condition_user_id a condition that looks for a specific user_id 
+# @param [String] session[:logged_in] the specific user_id
+#
+# @param [String] user the table name
+# @param [String] colum the column name and what it should be updated to
+# @param [String] condition_user_id a condition that has to be true
+# @param [String] value value of the condition 
+#
+# @param [String] library the tablename on the new table
+# @param [String] cart the name of the old table containing the data
+# @param [String] condition_user_id a condition that looks for a specific user_id 
+# @param [String] session[:logged_in] the specific user_id
+#
+# @param [String] cart the table name
+# @param [String] condition_user_id a condition that looks for a specific user_id 
+# @param [String] session[:logged_in] the specific user_id
+#
+# @see Model#join_from_db
+# @see Model#get_from_db
+# @see Model#update_db
+# @see Model#big_insert_into_db
+# @see Model#delete_db
 post("/library/new") do 
     user_cart = params[:user_cart].split('').map(&:to_i)
     prices = join_from_db("price", "quotes", "cart", "cart.quote_id = quotes.quote_id", "cart.user_id", session[:logged_in])
@@ -188,11 +334,24 @@ post("/library/new") do
     redirect("/cart/")
 end
 
+# Sets all session cokies to nil
+#
 post("/quotes") do
     session.destroy
     redirect("/")
 end
 
+# Creates a new quote 
+#
+# @param [Integer] session[:logged_in] the users id
+#
+# @param [String] quotes the table where the data should go
+# @param [String] columns which columns the data should go in
+# @param [String] "?,?,?,?" amount of columns in (?)
+# @param [Array] values containing the values to be put in the columns
+#
+# @see Model#is_admin
+# @see Model#insert_into_db
 post("/quotes/new") do
     quote = params[:quote]
     price = params[:price]
@@ -205,6 +364,14 @@ post("/quotes/new") do
     redirect("/quotes/new/")
 end
 
+# Updates the quote information for a specific quote
+#
+# @param [String] quotes the table name
+# @param [String] columns the columns name
+# @param [String] condition_quote_id a condition that looks for a specific quote_id
+# @param [String] quote_id the specific quote_id
+#
+# @see Model#update_db
 post("/quotes/edit/update") do
     quote_id = params[:update_button]
     new_quote = params[:new_quote]
@@ -215,12 +382,30 @@ post("/quotes/edit/update") do
     redirect("/quotes/edit/")
 end
 
+# Detetes a quote and all its information
+#
+# @param [String] quotes the table name
+# @param [String] condition_quote_id a condition that looks for a specific quote_id
+# @param [String] quote_id the specific quote_id
+#
+# @see Model#delete_db
 post("/quotes/edit/delete") do
     quote_id = params[:delete_button]
     delete_db("quotes", "quote_id", "#{quote_id}", "quote_id", "#{quote_id}")
     redirect("/quotes/edit/")
 end
 
+# Creates a new origin 
+#
+# @param [Integer] session[:logged_in] the users id
+#
+# @param [String] origin the table where the data should go
+# @param [String] columns which columns the data should go in
+# @param [String] "?,?" amount of columns in (?)
+# @param [Array] values containing the values to be put in the columns
+#
+# @see Model#is_admin
+# @see Model#insert_into_db
 post("/origin/new") do 
     person = params[:person]
     backstory = params[:backstory]
@@ -231,6 +416,14 @@ post("/origin/new") do
         redirect("/origin/new/")
 end
 
+# Updates the origin information for a specific origin
+#
+# @param [String] origin the table name
+# @param [String] columns the columnns name and what they should be
+# @param [String] condition_origin_id a condition that looks for a specific origin_id
+# @param [String] origin_id the specific origin_id 
+#
+# @see Model#update_db
 post("/origin/edit/update") do
     origin_id = params[:update_button]
     new_person = params[:new_person]
@@ -239,13 +432,31 @@ post("/origin/edit/update") do
     redirect("/origin/edit/")
 end
 
-
+# Detetes one item in the cart for the user
+#
+# @param [String] cart the table name
+# @param [String] condition_quote_id a condition that looks for a specific quote_id
+# @param [String] quote_id the specific quote_id
+#
+# @see Model#delete_db
 post("/cart") do 
     quote_id = params[:quote_id]
     delete_db("cart", "user_id", session[:logged_in], "quote_id", quote_id)
     redirect("/cart/")
 end
 
+# Adds a new quote to the shoppingcart
+#
+# @param [Integer] quote_id the quotes id to be checked
+# @param [Integer] user_id the id of the user
+#
+# @param [String] cart the table where the data should go
+# @param [String] columns which columns the data should go in
+# @param [String] "?,?" amount of columns in (?)
+# @param [Array] values containing the values to be put in the columns
+#
+#@see Model#owned_quote
+#@see Model#insert_into_db
 post("/cart/new") do 
     quote_id = params[:quote_id]
     if owned_quote(quote_id, session[:logged_in]) == false
--- Quotable/model.rb
+++ Quotable/model.rb
@@ -1,11 +1,25 @@
 require "bcrypt"
+# Makes commenting possible
+#
 module Model
+    # Connects to the specified database file
+    #
+    # @return [Array] containing databasefile
+    #
     def db()
         db = SQLite3::Database.new("db/quotables.db")
         db.results_as_hash = true
         return db
     end
 
+    # Verifies that the input data from a new user is correct 
+    #
+    # @param [String] username the inputed username
+    # @param [String] password the inputed user password
+    # @param [String] password_verify the inputed controller password
+    #
+    # @return [String] containing information about if the inputed values were allowed
+    #
     def input_chek(username, password, password_verify)
         if password != password_verify || username == "" || password == "" 
             return "input_false"                                    
@@ -19,6 +33,13 @@ module Model
         return BCrypt::Password.create(password)
     end
 
+    # Checks if the inputed password is the same as in the database
+    #
+    # @param [String] password the inputed password to the user
+    # @param [String] username the username that tries to log in
+    #
+    # @return [Boolean] returns true/false based on if the inputed password matches the registerd password 
+    #
     def password_checker(password, username)
         controll_password = get_from_db("password", "user", "username", username)[0]["password"]
         if BCrypt::Password.new(controll_password) == password
@@ -28,6 +49,12 @@ module Model
         end
     end
 
+    # Finds out how many times the user has tried to login in the last 60 seconds to protect from spam login
+    #
+    # @param [Array] time_array containing the times the user tries to login
+    #
+    # @return [Boolean] returns true/false based on if the user has tried to login more than 3 times in the last 60 seconds
+    #
     def time_checker(time_array)
         time_array.each do |time|
             if time_array[time_array.length - 1] - time > 60  
@@ -41,6 +68,12 @@ module Model
         end
     end
 
+    # Checks if the user is an admin
+    #
+    # @param [Integer] user_id the users id
+    #
+    # @return [Boolean] based on if the user is an admin
+    #
     def is_admin(user_id)
         admin = get_from_db("admin", "user", "user_id", session[:logged_in])[0]["admin"] if session[:logged_in] != nil
         if admin == 1 
@@ -50,6 +83,12 @@ module Model
         end
     end
 
+    # Checks if the inputed line is nil
+    #
+    # @param [String] line to be checked if nil
+    #
+    # @return [Boolean] based on if the inputed line was nil
+    #
     def is_nil(line)
         if line == nil
             return true
@@ -58,6 +97,13 @@ module Model
         end
     end
 
+    # Finds out if the user owns or has in cart a specific quote
+    #
+    # @param [Integer] quote_id the quotes id to be checked
+    # @param [Integer] user_id the id of the user
+    #
+    # @return [Boolean] whether the user owns/have in cart the quote
+    #
     def owned_quote(quote_id, user_id)
         owned_quote = get_from_db("quote_id", "library", "quote_id", "#{quote_id}", "user_id", "#{user_id}")[0]
         have_in_cart = get_from_db("quote_id", "cart", "quote_id", "#{quote_id}", "user_id", "#{user_id}")[0]
@@ -68,6 +114,17 @@ module Model
         end
     end
 
+    # Gets information out from the database based on inputed values
+    #
+    # @param [String] colum the column/columns that is to be selected
+    # @param [String] table the table that the information will come from
+    # @param [String] condition a optional condition to be checked 
+    # @param [String] value value of the condition to be checked
+    # @param [String] condition2 another optional condtition to be checked 
+    # @param [String] value2 vale of the second condition to be checked
+    #
+    # @return [Array] containing hash with the specified information from the database
+    #
     def get_from_db(colum, table, condition = nil, value = nil, condition2 = nil, value2 = nil)
         if condition2 == nil
             information = db.execute("SELECT #{colum} FROM #{table}#{condition == nil ? "" : " WHERE #{condition} LIKE ?"}", value)
@@ -77,22 +134,62 @@ module Model
         return information
     end
 
+    # Inserts inputed information to the database
+    #
+    # @param [String] table the table where the data should go
+    # @param [String] colums which column/columns the data should go in
+    # @param [String] numbers containing ? based on the amount of columns that where put in
+    # @param [Array] values containing the values to be put in the columns 
+    #
     def insert_into_db(table, colums, numbers, values)
         db.execute("INSERT INTO #{table} (#{colums}) VALUES(#{numbers})", values)
     end
 
+    # Copies data from one table to another
+    #
+    # @param [String] newtable the tablename on the new table
+    # @param [String] oldtable the name of the old table containing the data
+    # @param [String] condition a condition that has to be true
+    # @param [String] value the value of the condition
+    #
     def big_insert_into_db(newtable, oldtable, condition, value)
         db.execute("INSERT INTO #{newtable} SELECT * FROM #{oldtable} WHERE #{condition} LIKE ?", value)
     end
 
+    # Updates data in the database
+    #
+    # @param [String] table the table name
+    # @param [String] colum the column name and what it should be updated to
+    # @param [String] condition a condition that has to be true
+    # @param [String] value value of the condition 
+    #
     def update_db(table, colum, condition, value)
         db.execute("UPDATE #{table} SET #{colum} WHERE #{condition} LIKE ?", value)
     end
 
+    # Deletes specified data from the database
+    #
+    # @param [String] table the table name
+    # @param [String] condition1 a condition that needs to be true
+    # @param [String] value1 the value of the first condition
+    # @param [String] condition2 a second condition that needs to be true or a repeat of the first if not needed
+    # @param [String] value2 the value of the second condition or a repeat of the first if not needed
+    #
     def delete_db(table, condition1, value1, condition2, value2)
         db.execute("DELETE FROM #{table} WHERE #{condition1} LIKE ? AND #{condition2} LIKE ?", value1, value2)
     end
 
+    # Does a inner join funktion to take out information from two different tables
+    #
+    # @param [String] colums the column names 
+    # @param [String] table1 the name of the first table
+    # @param [String] table2 the name of the second table
+    # @param [String] togheter what they have togheter
+    # @param [String] condition a condition that needs to be true
+    # @param [String] value the value of the condition
+    #
+    # @return [Array] containing hash with the specified data
+    #
     def join_from_db(colums, table1, table2, togheter, condition, value)
         information = db.execute("SELECT #{colums} FROM #{table1} INNER JOIN #{table2} ON #{togheter} WHERE #{condition} LIKE ?", value)
         return information
